5acb316a5f7c26f52ce8211120ca1ce7d3e31113
Joseph61166 <Joseph61166@GB-LD1LPW10G028.mastek.com>
2019-07-26 14:45:30 +0100

    New Java project created

---
+++ farmerHome/src/main/java/com/example/farmerHome/FarmerHomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.farmerHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmerHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmerHomeApplication.class, args);
+	}
+
+}
