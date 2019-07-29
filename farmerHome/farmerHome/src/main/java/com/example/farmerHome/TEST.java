3c9e9646bb4d0b70ee7386094f84911ce65bf8e3
Monica61182 <Monica61182@GB-LD1LPW10G037.mastek.com>
2019-07-29 16:45:03 +0100

    PRODUCT: Create Entity, Lifecycle, Repository, Service class, JPA, and Test Cases; Manage Custom Queries and Test Cases for Product (fetch by price range, expiry date, and category); Test HTTP methods (@POST and @GET) with Postman

--- farmerHome/farmerHome/src/main/java/com/example/farmerHome/TEST.java
+++
@@ -1,5 +0,0 @@
-package com.example.farmerHome;
-
-public class TEST {
-
-}
---
+++ farmerHome/farmerHome/src/main/java/com/example/farmerHome/apis/APIConfig.java
@@ -0,0 +1,13 @@
+package com.example.farmerHome.apis;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+//create the Jersey server configuration class
+@Component
+public class APIConfig extends ResourceConfig {
+	public APIConfig() {
+		//register each service class in ResourceConfig
+		register(ProductService.class);
+	}
+}
