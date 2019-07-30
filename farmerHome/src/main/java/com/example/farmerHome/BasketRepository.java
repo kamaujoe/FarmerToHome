46e195d70130984ae1d49de690d7bac9d5946cbb
gregchappell <50618381+gregchappell@users.noreply.github.com>
2019-07-30 09:36:45 +0100

    Add files via upload

---
+++ farmerHome/src/main/java/com/example/farmerHome/BasketRepository.java
@@ -0,0 +1,13 @@
+package com.example.farmerHome.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
+
+import com.example.farmerHome.entities.Basket;
+
+@Component
+public interface BasketRepository
+	extends CrudRepository<Basket, Integer>{
+	
+
+}
