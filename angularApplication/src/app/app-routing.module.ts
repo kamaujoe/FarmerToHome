24826a32fab74f59e1343e3df2a1dc4a2d9a3215
Faith Elikirmawi <Faith61162@Mastek.Com>
2019-07-31 15:31:30 +0100

    commit update

--- angularApplication/src/app/app-routing.module.ts
+++ angularApplication/src/app/app-routing.module.ts
@@ -36,6 +36,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
