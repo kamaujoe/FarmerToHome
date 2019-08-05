51c417f181001915423cc562088c907b86ee67f3
Miles Smith <Miles61168@Mastek.Com>
2019-08-05 16:05:40 +0100

    Products drop down methods created on seller html

--- angularApplication/src/app/seller-profile/seller-profile.component.ts
+++ angularApplication/src/app/seller-profile/seller-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SellerService } from '../seller.service';
 import { Farmer } from '../farmer';
+import { Product } from '../basket/product';
 
 @Component({
   selector: 'app-seller-profile',
@@ -13,6 +14,11 @@ export class SellerProfileComponent implements OnInit {
   farmerName: String
   farmLocation: String
   products: String
+  
+  assignedProducts: Product[]
+
+  farmerProds: Product[]
+  selectProductId: number
 
   farmers: Farmer[]
 
@@ -23,18 +29,31 @@ export class SellerProfileComponent implements OnInit {
   isSellerFormVisable: boolean
 
   isSellerFormValid: boolean
-  invalidFormMessage: boolean
+  invalidFormMessage: String
+
+  isProductFormVisable:boolean
+  isProductFormValid:boolean
+
 
   
   constructor(private farmerSvc:SellerService) { 
+
     this.isEditable=false
     this.isSellerFormVisable=false
     this.isSellerFormValid=true
+
+    this.isProductFormVisable=false
+    this.isProductFormValid=true
 
     this.farmerId=3
     this.farmerName="Farmer Joe"
     this.farmLocation="Leeds"
     this.products="Whole Foods"
+    
+    this.assignedProducts =
+    [
+      {productId:4,productName:"Rice",quantity:1,expiry_date:22/12/2019,size:"Large",price:1.99,category:"BAKERY_DAIRY"}
+    ]
   }
 
   ngOnInit() {
@@ -48,6 +67,8 @@ export class SellerProfileComponent implements OnInit {
         this.farmerName = response.farmerName
         this.farmLocation = response.farmLocation
         this.products = response.products
+        
+        this.assignedProducts = response.assignedProducts
       }
     )
   }
@@ -57,10 +78,72 @@ export class SellerProfileComponent implements OnInit {
     this.updateSellerDetails()
   }
 
+  showProductForm() {
+    this.isProductFormVisable = true
+
+    this.loadFarmerProducts()
+  }
+
+  updateSelectedProductId(productId) {
+    this.selectProductId=productId
+  }
+
+  assignNewProduct() {
+    this.farmerSvc.assignProductToSeller(
+      this.farmerId,this.selectProductId
+    ).subscribe(
+      response => {
+        this.fetchCurrentSellerFromService()
+      }
+    )
+    this.isProductFormVisable=false
+  }
+
+  loadFarmerProducts() {
+    this.farmerSvc.loadAllProductsFromServer()
+        .subscribe(
+          response =>{
+            this.farmerProds = response
+          })
+  }
+
+  deleteProduct(index) {
+    this.assignedProducts.splice(index, 1)
+  }
+
+  addNewProduct(pproductId,pproductName,pprice,pquantity,psize,pexpiry_date,pcategory) {
+    if(isNaN(pproductId))
+    {
+      this.isProductFormValid=false
+      this.invalidFormMessage="Product ID must be a number"
+    }
+    else if(pproductName.length<4){
+      this.isProductFormValid=false
+      this.invalidFormMessage="Product name must be greater than 4 characters"
+    }
+    else {
+      this.assignedProducts.push({
+        productId:pproductId,
+        productName:pproductName,
+        price:pprice,
+        quantity:pquantity,
+        size:psize,
+        expiry_date:pexpiry_date,
+        category:pcategory
+      })
+      this.isProductFormVisable=false
+      this.isProductFormValid=true
+      this.invalidFormMessage=""
+    }
+
+  }
+
   updateSellerDetails() {
     this.farmerSvc.updateFarmerOnServer({
-      farmerId:this.farmerId, farmerName:this.farmerName,
-      farmLocation:this.farmLocation, products:this.products
+      farmerId:this.farmerId, 
+      farmerName:this.farmerName,
+      farmLocation:this.farmLocation, 
+      products:this.products,
     }).subscribe(
       response => {
         this.fetchCurrentSellerFromService()
