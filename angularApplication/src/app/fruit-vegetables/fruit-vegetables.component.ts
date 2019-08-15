f7c2c0977ea515476886d17f14ac378dfbdb6b78
Faith Elikirmawi <faith.elikirmawi@hotmail.co.uk>
2019-08-15 15:25:03 +0100

    updates

--- angularApplication/src/app/fruit-vegetables/fruit-vegetables.component.ts
+++ angularApplication/src/app/fruit-vegetables/fruit-vegetables.component.ts
@@ -1,17 +1,35 @@
+
 import { Component, OnInit } from '@angular/core';
 import { Products } from '../products';
 import { ProductsService } from '../products.service';
-import {Basket } from '../basket/basket';
+import { Basket } from '../basket/basket';
+import { Quantity } from '../basket/quantity';
+import { OrderService } from '../order.service';
+import { Order } from '../basket/order';
+import { BasketItemsService } from '../basket-items.service';
 
 @Component({
   selector: 'app-fruit-vegetables',
   templateUrl: './fruit-vegetables.component.html',
   styleUrls: ['./fruit-vegetables.component.css']
 })
+
 export class FruitVegetablesComponent implements OnInit {
-   products: Products[]
-   basketId : number
-   currentProduct : Basket
+    currentBasket : Basket[]
+    addQua : Order[]
+    currentQuantity : Quantity[]
+    updateQuantity : Quantity
+
+    AllQuantities : number[]
+    currentOrder : Order[]
+    quantity : number
+    orderId : number
+
+   
+    productId : number
+    basketId : number
+    products: Products[]
+    currentProduct : Order
 
    constructor(private productService: ProductsService) { 
     this.basketId = 75 
@@ -33,5 +51,4 @@ export class FruitVegetablesComponent implements OnInit {
       }
     )
   }
-
 }
